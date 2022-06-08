f391c689ef9e33a1355cad47e374439b47d6ea4f
Henry Merrilees <55256991+henry-merrilees@users.noreply.github.com>
2022-06-08 04:48:52 -0500

    Re-written Tweet Import

--- tweet_import.py
+++ tweet_import.py
@@ -1,169 +1,166 @@
+import collections
+import datetime
 import itertools
+
+import os
 from typing import Callable, Any
+from datetime import datetime
 from tqdm import tqdm
 from csv import DictReader, DictWriter, reader
 from pathlib import Path
 import re
 import subprocess
-INPUT_DIR = 'source'
-OUTPUT_DIR = 'cleaned'
+from collections.abc import Sized, Iterable, Iterator
+
+from typing import TypedDict
+
 OUTPUT_ENCODING = 'utf8'
 
-def get_encoding(filename):
-    '''Attempts to predict the encoding of filename.'''
-    out = subprocess.Popen([
-        'file',
-        '-I',
-        filename], subprocess.PIPE, subprocess.STDOUT, **('stdout', 'stderr')).communicate()[0]
+SOURCE_DIR = Path("source")
+
+
+def get_encoding(path) -> str:
+    """Attempts to predict the encoding of filename."""
+    out = subprocess.Popen(['file', '-I', path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out = out.communicate()[0]
     enc = out.lstrip().split()[2].decode().partition('=')[2]
     print(f'''Detected encoding: {enc}.''')
     return enc
 
 
 def nested_csvs(path):
-    '''Returns a list of csv files as pathlike objects.'''
+    """Returns a list of csv files as pathlike objects."""
     return Path(path).glob('**/*.csv')
 
 
-def import_tweets_from_file(*paths):
-Unsupported opcode: GEN_START
-    '''Generates tweets from path(s) to csvs as positional arguments.'''
-    pass
-# WARNING: Decompyle incomplete
+def import_from_file(*paths):
+    """Generates tweets from path(s) to csvs as positional arguments."""
+    for path in paths:
+        with open(path, encoding=get_encoding(path)) as csvfile:
+            for tweet in DictReader(csvfile):
+                yield tweet
 
 
-def import_tweets_from_dir(*dirs):
-    '''Generate all tweets from directory/ies w/ paths as positional arguments.'''
-    pass
-# WARNING: Decompyle incomplete
+def import_from_dir(*dirs):
+    """Generate all tweets from directory/ies w/ paths as positional arguments."""
+    for dir in dirs:
+        for path in nested_csvs(dir):
+            yield from import_from_file(path)
 
 
 def dedup_tweets(tweets):
-Unsupported opcode: GEN_START
-    '''Generates deduplicated tweets from tweets iterator.'''
-    pass
-# WARNING: Decompyle incomplete
+    """Generates deduplicated tweets from tweets iterator."""
+    ids = set()
+    for tweet in tweets:
+        id = tweet["user_id"]
+        if id in ids:
+            continue
+        else:
+            yield tweet
+            ids.add(id)
 
 
 def clean_text(text):
-    '''Returns cleaned text from a text input.'''
+    """Returns cleaned text from a text input."""
     text = re.sub('http\\S*\\b', '', text)
     text = re.sub('@\\S*\\b', '', text)
-    text = re.sub("[^\\w.,'?\\/\\[\\]()]", ' ', text)
+    text = re.sub("[^\\w.,'?/\\[\\]()]", ' ', text)
     text = re.sub("(?<=\\B)[.,'?]", '', text)
     text = re.sub('[\\s\\n]+', ' ', text)
     text = text.strip()
     return text
 
-
-def validate_tweet(tweet):
-    '''Bool check if tweet would not error on clean_tweets.
-    It appears that some tweets are filled with Nones. tweet_is_valid tests against this possibility.
-    If any other criterion for invalid tweets arise, they should be added to validate_tweet.'''
-    return not any((lambda .0: pass# WARNING: Decompyle incomplete
-)(tweet.values()))
-
-
-def validate_tweets(tweets):
-Unsupported opcode: GEN_START
-    '''Filters to retain valid tweets from tweets iterator.'''
-    pass
-# WARNING: Decompyle incomplete
-
-
-def clean_tweets(tweets):
-Unsupported opcode: GEN_START
-    '''Generates new tweets updated with "clean" item for each in a tweets iterator. Skips invalid tweets.'''
-    pass
-# WARNING: Decompyle incomplete
-
-
-def _cleaner_gen_tqdm(tweets):
-Unsupported opcode: GEN_START
-    '''Helper function for clean_tweets_eager.
-\tGenerates new tweets updated with "clean" item for each in a tweets list. Skips invalid tweets.'''
-    pass
-# WARNING: Decompyle incomplete
-
-
-def clean_tweets_eager(tweets):
-    '''Adds a cleaned field to all tweets, and returns a list excluding those invalid as defined by validate_tweet.'''
-    return list(_cleaner_gen_tqdm(tweets))
+def clean_tweet(tweet):
+    """Cleans the text of the in-place tweet."""
+    return tweet | {"clean": clean_text(tweet["text"])}
 
 
 def make_encodable_text(text):
-    '''Makes text encodable to output via brute force.'''
+    """Makes text encodable to output via brute force."""
     return text.encode(OUTPUT_ENCODING, 'ignore').decode(OUTPUT_ENCODING)
 
 
-def make_encodable_tweet(tweet):
-    '''Brute forces tweet compatability with OUTPUT_ENCODING.'''
-Unsupported opcode: GEN_START
-    return dict((lambda .0: pass# WARNING: Decompyle incomplete
-)(tweet.items()))
+def make_encodable_values_tweet(tweet):
+    """Brute forces tweet values compatability with OUTPUT_ENCODING."""
+    return {k: make_encodable_text(v) for k, v in tweet.items()}
 
 
-def make_encodable_tweets(tweets):
-    '''Generates tweets compatible with OUTPUT_ENCODING from tweets iterable.'''
-    return map(make_encodable_tweet, tweets)
+def validate_tweet(tweet):
+    """
+    Ensure all fields are populated.
+    """
+    return all(tweet.values())
 
 
-def tweet_writer(path):
-Unsupported opcode: GEN_START
-    '''Coroutine for writing into path (csv file).
-
-\t# init:
-\twriter = tweet_writer(path)
-\tnext(writer)
-
-\t# use:
-\twriter.send(tweet)
-\t'''
-    pass
-# WARNING: Decompyle incomplete
+def process_tweet(tweet):
+    tweet = clean_tweet(tweet)
+    tweet = make_encodable_values_tweet(tweet)
+    valid = validate_tweet(tweet)
+    return tweet, valid
 
 
-def write_tweets(encodable_tweets, path):
-Unsupported opcode: GEN_START
-    '''write '''
-    pass
-# WARNING: Decompyle incomplete
+def process_tweets(tweets):
+    
+    for tweet in tweets:
+        try:
+            tweet, valid = process_tweet(tweet)
+            if valid:
+                yield tweet
+            else:
+                continue
+        except TypeError:
+            continue
 
 
-def process_tweets(tweets, path):
-Unsupported opcode: GEN_START
-    pass
-# WARNING: Decompyle incomplete
+def write_tweets(tweets, path):
+    """
+    WARNING:
+    DO NOT NEST import_tweets_from_file INSIDE write_tweets
+    (unless you want the imported file to be immediately overwritten, I guess?);
+    solution: write_tweets(list(import_tweets_from_file(_)), _).
+    """
+    fieldnames = ["user_name", "clean", "tweet_id", "text", "user_id", "created_at", "subscription_id", "subscription_name"]
+    os.makedirs(path.parent, exist_ok=True)
+    with open(path, "w+") as f:
+        writer = DictWriter(f, fieldnames)
+        writer.writeheader()
+        for tweet in tweets:
+            writer.writerow(tweet)
+            yield tweet
 
 
-def process_tweets_eager(tweets, path):
-    tweets = list(validate_tweets(tqdm(tweets)))
-    tweets = list(dedup_tweets(tqdm(tweets)))
-    tweets = list(clean_tweets(tqdm(tweets)))
-    tweets = list(make_encodable_tweets(tqdm(tweets)))
-    tweets = list(write_tweets(tqdm(tweets), path))
-    return tweets
+def load(input_dir, output_file=None, filter_by=None, limit=None):
+    tweets = import_from_dir(input_dir)
+    tweets = dedup_tweets(tweets)
+    tweets = process_tweets(tweets)
+    if filter_by:
+        tweets = filter(filter_by, tweets)
+    if output_file:
+        tweets = write_tweets(tweets, output_file)
+    tweets = itertools.islice(tweets, limit)
+    yield from tweets
 
 
-def import_from_source():
-    all_tweets = []
-    return all_tweets
-
-from itertools import filterfalse, tee
-
-def load_pipeline():
-Unsupported opcode: GEN_START
-    pass
-# WARNING: Decompyle incomplete
+def load_list(input_dir, output_file=None, filter_by=None, limit=None):
+    """
+    Returns a list of tweets obtained by load.
+    Useful when you want eager execution and to keep the results.
+    See load for more details.
+    """
+    return list(load(input_dir, output_file, filter_by, limit))
 
 
-def load(limit = (None,)):
-Unsupported opcode: GEN_START
-    '''Loads tweets subject to limit from CSVs.'''
-    pass
-# WARNING: Decompyle incomplete
+def load_deque(input_dir, output_file, filter_by=None, limit=None):
+    """
+    Exhausts tweets generator obtained by load. Returns None.
+    Useful when you want eager execution to save results to a file, but don't need said results in python.
+    See load for more details.
+    """
+    collections.deque(load(input_dir, filter_by, limit), maxlen=0)
+
+
+
 
 if __name__ == '__main__':
     list(tqdm(load()))
     print('Done!')
-    return None
