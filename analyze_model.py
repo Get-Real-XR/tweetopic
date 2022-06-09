b05b4c5a67199d763bea2825cb6c088dab82882a
Henry Merrilees <55256991+henry-merrilees@users.noreply.github.com>
2022-06-08 19:07:56 -0500

    Moved visualization functions to visualize.py

--- analyze_model.py
+++ analyze_model.py
@@ -7,7 +7,7 @@ import tweet_wrangling
 
 import visualize
 
-models = list(Path("data").glob("**/*.model"))
+models = list(Path("data").glob("**/*.reducedmodel"))
 most_recent_model = max(models)
 stamp = most_recent_model.stem
 tweets_path = Path("data").glob(f"**/{stamp}.csv")
@@ -15,25 +15,7 @@ tweets_path = Path("data").glob(f"**/{stamp}.csv")
 topic_model: BERTopic = BERTopic.load(str(most_recent_model))
 tweets = tweet_wrangling.import_from_file(*tweets_path)
 
-docs, topics, probabilities = tweet_wrangling.dict_zip(tweets, ["docs", "topics", "probabilities"]).values()
 
-figures = [
-    topic_model.visualize_topics(top_n_topics=30),
-    topic_model.visualize_hierarchy(),
-    topic_model.visualize_topics(),
-    topic_model.visualize_term_rank(),
-    topic_model.visualize_distribution(),
-    topic_model.visualize_hierarchy(),
-    topic_model.visualize_heatmap(),
-    topic_model.visualize_barchart()]
-
-topic_model.visualize_topics_over_time(),
-
-topic_model.visualize_topics_per_class(),
-# topic_model.visualize_distribution(probabilities)]
-
-
-
-visualize.visualize_modeL(topic_model)
+visualize.visualize_modeL(topic_model, tweets)
 # topic_model.reduce_topics(*data.values(), nr_topics=30)
 
